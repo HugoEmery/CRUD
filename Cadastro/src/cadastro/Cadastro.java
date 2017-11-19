00b8d679fb78ab3056c9de0838db2be809306608
hugoec <emery_hugo@hotmail.com>
2017-11-19 16:09:56 -0300

    first commit

---
+++ Cadastro/src/cadastro/Cadastro.java
@@ -0,0 +1,27 @@
+//******************************************************
+//Instituto Federal de São Paulo - Campus Sertãozinho
+//Disciplina......: M3LPBD
+//Programação de Computadores e Dispositivos Móveis
+//Aluno...........: Hugo Emery Costa
+//******************************************************
+package cadastro;
+
+import Telas.TelaCadastro;
+import Telas.TelaExcluir;
+import Telas.TelaInicial;
+import Telas.ListaJFrame;
+
+/**
+ *
+ * @author Hugo
+ */
+public class Cadastro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new TelaInicial().setVisible(true);//Habilita a Tela inicial
+    }
+    
+}
